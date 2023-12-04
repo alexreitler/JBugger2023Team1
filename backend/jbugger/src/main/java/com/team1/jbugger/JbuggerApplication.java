dce38cace086ba3ac8ad6b6ac03d9443a6ebeca0
Alex Reitler <reitleralex@gmail.com>
2023-12-04 11:48:43 +0200

    Initialised frameworks

---
+++ backend/jbugger/src/main/java/com/team1/jbugger/JbuggerApplication.java
@@ -0,0 +1,13 @@
+package com.team1.jbugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbuggerApplication.class, args);
+	}
+
+}
---
+++ backend/jbugger/src/test/java/com/team1/jbugger/JbuggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.jbugger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbuggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
